17fa895e2dedb6ed8d800d4aced742f09cfa5123
Vadim Ponomarev <vadim.v.ponomarev@gmail.com>
2020-10-06 00:16:04 +0300

    New object - VoiceRoom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import javax.security.auth.login.LoginException;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,8 @@ public class Main {
             JDA jda = JDABuilder.createDefault(args[0])
                     .addEventListeners(new NewRooms())
                     .build();
+            System.out.println("Bot started at: " + LocalDateTime.now().toString());
+
         }catch (LoginException e){
             System.out.print("JDA build fail");
             e.printStackTrace();
---
+++ src/main/java/VoiceRoom.java
@@ -0,0 +1,46 @@
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+
+import java.util.Objects;
+
+public class VoiceRoom {
+    private String GuildID;
+    private String VoiceChannelID;
+
+    private boolean locked = false;
+
+    public VoiceRoom(String GuildID, String VoiceChannelID) {
+        this.GuildID = GuildID;
+        this.VoiceChannelID = VoiceChannelID;
+    }
+
+    public VoiceRoom(Guild guild, VoiceChannel channel){
+        this(guild.getId(), channel.getId());
+    }
+
+    public String getGuildID() {
+        return GuildID;
+    }
+
+    public String getVoiceChannelID() {
+        return VoiceChannelID;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VoiceRoom voiceRoom = (VoiceRoom) o;
+        return GuildID.equals(voiceRoom.GuildID) &&
+                VoiceChannelID.equals(voiceRoom.VoiceChannelID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(GuildID, VoiceChannelID);
+    }
+}
